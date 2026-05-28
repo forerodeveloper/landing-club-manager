5e1deb1ef6df9ebd7ef532bba2c25c41b1f5c16a
jefersonforero <jeferson.forero@alus.com.mx>
2026-05-28 15:30:08 -0500

    feat: se mejora el CEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwind from '@tailwindcss/vite';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
     site: 'https://www.clubsportmanager.com',
+    integrations: [sitemap()],
     vite: {
         plugins: [
             tailwind(),
